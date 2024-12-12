2d8750ce1c558c7652c76264abddd0cb53773999
seyeoyadiran <seyeoyadiran@gmail.com>
2024-12-12 15:38:42 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Nav } from './components/Nav'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Nav/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,7 @@
+export const Nav = function() {
+    return(
+        <div className="nav">
+            Star Wars Starships
+        </div>
+    )
+}
